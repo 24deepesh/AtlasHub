9bf490d3a262cbe49b601c24425b33415a46cf23
DeepeshShukla <deepeshshukla471@gmail.com>
2025-10-07 23:24:41 +0530

    Initial commit for Atlas Hub project

---
+++ src/components/UI/HeroSection.jsx
@@ -0,0 +1,39 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+export const HeroSection = () => {
+
+  const navigate = useNavigate();
+  return (
+    
+    <main className="hero-section main">
+      <div className="container grid grid-two-cols">
+        <div className="hero-content">
+          <h1 className="heading-xl">
+            Explore the World, One Country at a Time.
+          </h1>
+          <p className="paragraph">
+            Discover the history, culture, and beauty of every nation. Sort,
+            search, and filter through countries to find the details you need.
+          </p>
+          {/* <button className="btn btn-darken btn-inline bg-white-box">
+            Start Exploring <FaLongArrowAltRight />
+          </button> */}
+          <button
+            className="btn btn-darken btn-inline bg-white-box"
+            onClick={() => navigate("/country")}
+          >
+            Start Exploring <FaLongArrowAltRight />
+          </button>
+        </div>
+        <div className="hero-image">
+          <img
+            src="https://res.cloudinary.com/dmefglt2d/image/upload/v1758553672/world_blld0y.png"
+            alt="world is beauty"
+            className="banner-image"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
